2af74e8f594138b6142fb62eedd255c8f514e61f
seungdeng17 <seungdeng17@gmail.com>
2023-02-05 20:05:32 +0900

    feat: 노말 타입 씬 반영, 기타 수정

--- js/main.js
+++ js/main.js
@@ -61,7 +61,11 @@
   const setLayout = () => {
     // 각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
-      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      if (sceneInfo[i].type === 'sticky') {
+        sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      } else if (sceneInfo[i].type === 'normal') {
+        sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight;
+      }
       sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
 
