419331eefeb35f8faa5365bd6d4c35383b0f3690
seungdeng17 <seungdeng17@gmail.com>
2023-02-05 18:56:00 +0900

    feat: scene1 main-message 애니메이션 적용1

--- js/main.js
+++ js/main.js
@@ -2,42 +2,50 @@
   let yOffset = 0; // window.pageYOffset 대신 쓸 변수
   let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이 합
   let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+  let enterNewScene = false; // 새로운 scene이 시작된 순간 true
 
   const sceneInfo = [
     {
       // 0
-      type: "sticky",
+      type: 'sticky',
       heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
       scrollHeight: 0,
       objs: {
-        container: document.querySelector("#scroll-section-0"),
+        container: document.querySelector('#scroll-section-0'),
+        messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+        messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+      },
+      values: {
+        messageA_opacity: [0, 1],
       },
     },
     {
       // 1
-      type: "normal",
+      type: 'normal',
       heightNum: 5,
       scrollHeight: 0,
       objs: {
-        container: document.querySelector("#scroll-section-1"),
+        container: document.querySelector('#scroll-section-1'),
       },
     },
     {
       // 2
-      type: "sticky",
+      type: 'sticky',
       heightNum: 5,
       scrollHeight: 0,
       objs: {
-        container: document.querySelector("#scroll-section-2"),
+        container: document.querySelector('#scroll-section-2'),
       },
     },
     {
       // 3
-      type: "sticky",
+      type: 'sticky',
       heightNum: 5,
       scrollHeight: 0,
       objs: {
-        container: document.querySelector("#scroll-section-3"),
+        container: document.querySelector('#scroll-section-3'),
       },
     },
   ];
@@ -46,9 +54,7 @@
     // 각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[
-        i
-      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
 
     yOffset = window.pageYOffset;
@@ -60,30 +66,64 @@
         break;
       }
     }
-    document.body.setAttribute("id", `show-scene-${currentScene}`);
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
   };
 
   const scrollLoop = () => {
+    enterNewScene = false;
     prevScrollHeight = 0;
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += sceneInfo[i].scrollHeight;
     }
 
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
       currentScene++;
-      document.body.setAttribute("id", `show-scene-${currentScene}`);
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
+
     if (yOffset < prevScrollHeight) {
+      enterNewScene = true;
       if (currentScene === 0) return; // 브라우저 바운스 효과로 인한 오류 방지
       currentScene--;
-      document.body.setAttribute("id", `show-scene-${currentScene}`);
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    }
+
+    if (enterNewScene) return;
+    playAnimation();
+  };
+
+  const playAnimation = () => {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
+        objs.messageA.style.opacity = messageA_opacity_in;
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
     }
   };
 
-  window.addEventListener("scroll", () => {
+  const calcValues = (values, currentYOffset) => {
+    let rv;
+    // 현재 씬(scroll-section)에서 스크롤된 범위를 비율로 구하기
+    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+    rv = scrollRatio * (values[1] - values[0]) + values[0];
+    return rv;
+  };
+
+  window.addEventListener('scroll', () => {
     yOffset = window.pageYOffset;
     scrollLoop();
   });
-  window.addEventListener("load", setLayout);
-  window.addEventListener("resize", setLayout);
+  window.addEventListener('load', setLayout);
+  window.addEventListener('resize', setLayout);
 })();
